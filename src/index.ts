12022d3f6ce4d1f8bec8011eaaacc3f094f3ee4a
unknown <adrian.alaszewski@billennium.com>
2021-07-05 16:18:15 +0200

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
-import * as _ from 'lodash'
 import './styles/style.sass'
 import file from './file.html'
 
 function component() {
     const element = document.createElement('div')
     element.classList.add('test')
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-
+    element.innerHTML = 'nothing is here'
     return element
 }
 
