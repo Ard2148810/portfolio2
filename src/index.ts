0e0d2ebe2ae892312ee16b165ce312b23a0e1105
unknown <adrian.alaszewski@billennium.com>
2021-07-07 15:23:37 +0200

    Basic slider functionality

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import headerHtml from './header.html'
 import navHtml from './nav.html'
 import mainHtml from './main.html'
 import footerHtml from './footer.html'
+import { Slider } from './slider/slider'
+import img from './img/navbg.png'
 
 class App {
     app: HTMLDivElement
@@ -22,3 +24,23 @@ class App {
 const app = new App()
 let contents = [headerHtml, navHtml, mainHtml, footerHtml]
 contents.forEach((item) => app.insertHtml(item))
+
+let sliderContent = [
+    {
+        imgSrc: img,
+        title: 'Tile',
+        subtitle: '0',
+    },
+    {
+        imgSrc: img,
+        title: 'Another one',
+        subtitle: '1',
+    },
+    {
+        imgSrc: img,
+        title: 'Even more',
+        subtitle: '2',
+    },
+]
+let slider = new Slider.Slider(sliderContent)
+document.getElementById('sliderTarget').appendChild(slider.getSliderElem())
--- src/slider/slider.ts
+++ src/slider/slider.ts
@@ -1,1 +1,84 @@
-class Slider {}
+import './slider.sass'
+
+export module Slider {
+    export class Slider {
+        private sliderElem: HTMLElement
+        private slideContainerElem: HTMLElement
+        private items: Array<SliderItem>
+        private animHandle: NodeJS.Timer
+
+        constructor(items: Array<SliderItem>) {
+            this.createSlider()
+            this.setSliderItems(items)
+            this.setAnimating(true)
+        }
+
+        private animate(slidingTarget: HTMLElement) {
+            let current = parseInt(slidingTarget.dataset.active)
+            let count = slidingTarget.childElementCount
+            current = (current + 1) % count // Calculate new index
+            if (current === 0) {
+                // Remove temporarily transition for loop effect
+                slidingTarget.classList.add('notransition')
+            } else {
+                slidingTarget.classList.remove('notransition')
+            }
+            slidingTarget.dataset.active = current.toString()
+            slidingTarget.style.transform = `translateX(-${current * 100}%)`
+        }
+
+        private createSlider() {
+            let elem = document.createElement('div')
+            elem.classList.add('slider')
+            let slideContainer = document.createElement('div')
+            slideContainer.classList.add('slider-container')
+            slideContainer.dataset.active = '0' // Keep active item index in the slider as data attribute
+            elem.appendChild(slideContainer)
+            this.slideContainerElem = slideContainer
+            this.sliderElem = elem
+        }
+
+        setSliderItems(items: Array<SliderItem>): HTMLElement {
+            this.items = items
+            items.push(items[0]) // Add first item at the end for sliding loop effect
+            console.log(items)
+            let htmlItems = ''
+            items.forEach((item) => {
+                htmlItems += `
+                <div class='slider-item'>
+                    <div class='slider-item-img' style='background: url("${item.imgSrc}")'></div>
+                    <div class='slider-label'>
+                        <p>${item.title}</p>
+                        <p class='slider-item-subtitle'>${item.subtitle}</p>
+                    </div>
+                </div>`
+            })
+            this.slideContainerElem.innerHTML = htmlItems
+            return this.sliderElem
+        }
+
+        getSliderElem(): HTMLElement {
+            return this.sliderElem
+        }
+
+        setAnimating(value: boolean) {
+            if (value) {
+                this.animHandle = setInterval(
+                    this.animate,
+                    3000,
+                    this.slideContainerElem
+                )
+            } else {
+                if (this.animHandle) {
+                    clearInterval(this.animHandle)
+                }
+            }
+        }
+    }
+
+    export interface SliderItem {
+        imgSrc: string
+        title: string
+        subtitle: string
+    }
+}
